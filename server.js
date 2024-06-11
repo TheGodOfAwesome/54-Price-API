50825dc53c11b91ac22b952038b480393daff428
Kuzivakwashe Muvezwa <muvezwakuzivakwashe@yahoo.com>
2024-06-11 14:00:43 +0200

    first push

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const mongoose = require('mongoose');
+const Post = require('./models/postModel')
 const port = process.env.PORT || 4000;
 
 app.use(express.json())
@@ -8,6 +9,5 @@ app.use(express.json())
 // routes
 
 app.get('/', (req, res) => {
-    // res.send('Get API')
-    res.end('535.66');
+    res.send('Get API')
 })
